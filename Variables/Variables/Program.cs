9dca2da7c4f56e9bd4f870765080539bb264b7d6
n0miya <dulapah6@gmail.com>
2021-05-20 09:54:18 +0700

    Variables

--- Variables/Variables/Program.cs
+++ Variables/Variables/Program.cs
@@ -10,6 +10,15 @@ namespace Variables
     {
         static void Main(string[] args)
         {
+            int x = 10;
+            int y = 5;
+            int z = x + y;
+            float money = 200.50f; // Use f to specify that it is float
+            double moeny2 = 100.50; // Double can store more value
+            string text = "Judge";
+            bool isMale = true;
+            Console.WriteLine(z);
+            Console.ReadLine();
         }
     }
 }
