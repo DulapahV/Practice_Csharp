a382edee6d4fee309c29403e3341aec8203212d8
n0miya <dulapah6@gmail.com>
2021-05-20 10:15:54 +0700

    Convert Data Type

---
+++ Convert Data Type/Convert Data Type/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convert_Data_Type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            double salary = double.Parse(Console.ReadLine());
+            Console.WriteLine("Salary: " + salary.ToString());
+            Console.ReadLine();
+        }
+    }
+}
