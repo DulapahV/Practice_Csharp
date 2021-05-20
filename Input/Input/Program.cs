b7798aae766770a91db78d58f3d858b9eb078d9a
n0miya <dulapah6@gmail.com>
2021-05-20 09:36:30 +0700

    Input

--- Input/Input/Program.cs
+++ Input/Input/Program.cs
@@ -10,6 +10,8 @@ namespace Input
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello" + " " + Console.ReadLine());
+            Console.ReadLine();
         }
     }
 }
