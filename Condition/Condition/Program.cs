66dbf78b48ea3e5ff319fea78f5f6d1f5858c5d5
n0miya <dulapah6@gmail.com>
2021-05-21 13:33:39 +0700

    Condition

---
+++ Condition/Condition/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            if (10 == 10)
+            {
+                Console.WriteLine("True");
+            }
+            Console.WriteLine("Bye");
+            Console.ReadLine();
+        }
+    }
+}
