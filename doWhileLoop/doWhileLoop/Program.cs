6aa85382d962ae4a8053caa90e683782636c4d3a
n0miya <dulapah6@gmail.com>
2021-05-21 14:44:28 +0700

    DoWhileLoop

---
+++ doWhileLoop/doWhileLoop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doWhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Multiplication of: ");
+            int multiplication = int.Parse(Console.ReadLine());
+            for (int i = 1; i < 13; i++)
+            {
+                Console.WriteLine(multiplication + " x " + i + " = " + multiplication * i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
