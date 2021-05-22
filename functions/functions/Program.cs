4ba818c732338c7a4f1306ab1ced4ddf847a1ee5
n0miya <dulapah6@gmail.com>
2021-05-22 12:23:23 +0700

    functions

---
+++ functions/functions/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sayHelloWorld();
+            Console.WriteLine(calculate());
+            Console.ReadLine();
+        }
+
+        // Private means that it can only execute in current class
+        // <Access Specifier> <Return Type> <Method Name>(Parameter List)
+        private static void sayHelloWorld()
+        {
+            for (int i = 0; i < 10; i++)
+            Console.WriteLine("Hello World");
+        }
+
+        private static int calculate()
+        {
+            int a = 10;
+            int b = 5;
+            int sum = a + b;
+            return sum;
+        }
+    }
+}
