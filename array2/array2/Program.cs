1ed1108a013bd85d73f9f49ec119345c9ca92eff
n0miya <dulapah6@gmail.com>
2021-05-22 10:21:59 +0700

    array (with initial data)

---
+++ array2/array2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ages = {18, 12, 14, 16, 28, 30};
+            string[] countries = { "Thailand", "Japan", "Laos" };
+            Console.WriteLine(ages[0]);
+            Console.WriteLine(countries[1]);
+            Console.ReadLine();
+        }
+    }
+}
