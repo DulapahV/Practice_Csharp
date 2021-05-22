2205ab61dd53c3733b0305bd483ffced91682fc5
n0miya <dulapah6@gmail.com>
2021-05-22 10:18:46 +0700

    array (no initial data)

---
+++ array/array/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ages = new int[10];
+            ages[0] = 100;
+            Console.WriteLine(ages[0]);
+            Console.ReadLine();
+        }
+    }
+}
