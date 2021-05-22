72540cf04b7077c5fc5f333413a6f0c57718e2bb
n0miya <dulapah6@gmail.com>
2021-05-22 11:49:19 +0700

    2D array (with initial data)

---
+++ 2D array example/2D array example/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2D_array_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] customers = { { "GG", "WP" }, { "Lol", "Nice" } };
+            string[][] users = new string[5][];
+            users[0] = new string[3];
+            users[1] = new string[4];
+        }
+    }
+}
