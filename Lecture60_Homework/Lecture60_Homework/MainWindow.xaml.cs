d5b4bf3f1a2b5ded0060b6c720ccb74e4eb803d7
n0miya <dulapah6@gmail.com>
2021-05-23 12:54:59 +0700

    Lecture60_Homework

---
+++ Lecture60_Homework/Lecture60_Homework/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lecture60_Homework
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_calculate_Click(object sender, RoutedEventArgs e)
+        {
+            int difference = int.Parse(inputIncome.Text) - int.Parse(inputExpense.Text);
+            int result = int.Parse(inputCost.Text) / difference;
+            resultDays.Text = result.ToString();
+        }
+    }
+}
