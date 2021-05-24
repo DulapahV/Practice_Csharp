3c469458ddd39bfedc2a61266e360c8991bc00db
n0miya <dulapah6@gmail.com>
2021-05-24 12:56:03 +0700

    OOP2

---
+++ OOP2/OOP2/Animal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    class Animal
+    {
+        int sizeOfAnimal = 0;
+
+        public int SizeOfAnimal { get => sizeOfAnimal; set => sizeOfAnimal = value; }
+
+        public string breathing()
+        {
+            return "I'm OK!";
+        }
+        public string move()
+        {
+                return "GO GO GO!";
+        }
+    }   
+}
---
+++ OOP2/OOP2/Cat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    class Cat : Animal
+    {
+        string name;
+
+        public string Name { get => name; set => name = value; }
+
+        public string cry()
+        {
+            return "Meow!";
+        }
+    }
+}
