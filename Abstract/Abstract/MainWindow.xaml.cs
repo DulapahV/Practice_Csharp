73b1c751d4f991d7468982bbf7b48d6f6c09ef21
n0miya <dulapah6@gmail.com>
2021-05-24 14:37:11 +0700

    Abstract

---
+++ Abstract/Abstract/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Abstract
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Square square = new Square();
+            Square square2 = new Square();
+            square.Side = 5;
+            square2.Side = 10;
+            MessageBox.Show(square.Area().ToString());
+        }
+    }
+}
