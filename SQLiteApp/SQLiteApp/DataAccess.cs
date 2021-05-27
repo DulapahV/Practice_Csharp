aad5afabe3de81b228e4e6019c490de1e5bfc4c9
n0miya <dulapah6@gmail.com>
2021-05-27 12:31:54 +0700

    SQLite

--- SQLiteApp/SQLiteApp/DataAccess.cs
+++ SQLiteApp/SQLiteApp/DataAccess.cs
@@ -9,21 +9,71 @@ namespace SQLiteApp
 {
     class DataAccess
     {
-        public static void InitializeDatabase()
+        public const string dbpath = "Customer.db";  //Declare default path
+        private static object createTable;
+
+        public static void InitializeDatabase() //Create table
         {
-            using (SqliteConnection db =
-               new SqliteConnection("Filename=sqliteSample.db"))
+            using (SqliteConnection db = new SqliteConnection($"Filename={dbpath}"))
             {
                 db.Open();
 
                 String tableCommand = "CREATE TABLE IF NOT " +
-                    "EXISTS MyTable (Primary_Key INTEGER PRIMARY KEY, " +
-                    "Text_Entry NVARCHAR(2048) NULL)";
-
+                    "EXISTS CustomerTable (uid INTEGER PRIMARY KEY, " +
+                    "first_Name NVARCHAR(50) NULL, " +
+                    "last_Name NVARCHAR(50) NULL, " +
+                    "email NVARCHAR(50) NULL)";
                 SqliteCommand createTable = new SqliteCommand(tableCommand, db);
 
                 createTable.ExecuteReader();
+
             }
+        }
+        public static void AddData(string firstName, string lastName, string email)
+        {
+            using (SqliteConnection db = new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+
+                SqliteCommand insertCommand = new SqliteCommand();
+                insertCommand.Connection = db;
+
+                // Use parameterized query to prevent SQL injection attacks
+                insertCommand.CommandText = "INSERT INTO CustomerTable VALUES (NULL, @first_Name, @last_Name, @email);";
+                insertCommand.Parameters.AddWithValue("@first_Name", firstName);
+                insertCommand.Parameters.AddWithValue("@last_Name", lastName);
+                insertCommand.Parameters.AddWithValue("@email", email);
+
+                insertCommand.ExecuteReader();
+
+                db.Close();
+            }
+
+        }
+        public static List<String> GetData() //Get data from database
+        {
+            List<String> entries = new List<string>();
+
+            using (SqliteConnection db =
+               new SqliteConnection($"Filename={dbpath}"))
+            {
+                db.Open();
+
+                SqliteCommand selectCommand = new SqliteCommand
+                    ("SELECT first_Name, last_Name, email from CustomerTable", db);
+
+
+                SqliteDataReader query = selectCommand.ExecuteReader();
+
+                while (query.Read())
+                {
+                    entries.Add($"{query.GetString(0)} {query.GetString(1)} ({query.GetString(2)})");
+                }
+
+                db.Close();
+            }
+
+            return entries;
         }
     }
 }
--- SQLiteApp/SQLiteApp/MainWindow.xaml.cs
+++ SQLiteApp/SQLiteApp/MainWindow.xaml.cs
@@ -25,5 +25,23 @@ namespace SQLiteApp
             InitializeComponent();
             DataAccess.InitializeDatabase();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string textResult = "";
+            List<string> getData = DataAccess.GetData();
+            for (int i = 0; i < getData.Count; i++)
+            {
+                textResult += getData[i] + "\n";
+            }
+            MessageBox.Show(textResult);
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            DataAccess.AddData(firstNameBox.Text, lastNameBox.Text, emailBox.Text);
+            firstNameBox.Text = lastNameBox.Text = emailBox.Text = "";
+        }
     }
 }
